2c4237f14106f057dbfeab86f49b25cc67ba183a
Elliot <elliotmrgn@gmail.com>
2020-05-18 18:02:13 -0400

    added delete

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -41,4 +41,19 @@ $(function() {
       }
     );
   });
+
+  $(".delete-btn").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted cat", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 });
