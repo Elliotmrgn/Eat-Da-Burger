e530c95f39306aa44fb72efdfcb87ee70fe85385
Elliot <elliotmrgn@gmail.com>
2020-05-18 13:44:22 -0400

    basics working

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,58 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".devour-btn").on("click", function(event) {
+    const id = $(this).data("id");
+   
+    const devour = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax(`/api/burgers/${id}`, {
+      type: "PUT",
+      data: devour
+    }).then(
+      function() {
+        console.log("BURGER DEVOURED");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    const newBurger = {
+      name: $("#newBurg").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+ /*  $(".delete-burger").on("click", function(event) {
+    const id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted cat", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  }); */
+});
